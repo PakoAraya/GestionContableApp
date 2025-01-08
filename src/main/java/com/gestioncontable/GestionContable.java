204729ce2a6739e922de1157550f7a16dcc46ca6
franarayah <franarayah@gmail.com>
2025-01-08 20:56:24 -0300

    Se termina el menu de la aplicacion

--- src/main/java/com/gestioncontable/GestionContable.java
+++ src/main/java/com/gestioncontable/GestionContable.java
@@ -21,9 +21,71 @@ public class GestionContable {
       System.out.println("Por favor selecciona una opcion: ");
       int opcion = scan.nextInt();
 
-
+      switch (opcion){
+        case 1:
+          totalIngresos = registrarTransaccion(scan, totalIngresos, totalEgresos);
+          break;
+        case 2:
+          mostrarTransacciones();
+          break;
+        case 3:
+          calcularTotalOperaciones(totalIngresos, totalEgresos);
+          break;
+        case 4:
+          calcularBalance(totalIngresos, totalEgresos);
+          break;
+        case 5:
+          System.out.println("=================================================");
+          System.out.println("==================== HASTA LUEGO =================");
+          System.exit(0);
+          break;
+        default:
+          System.out.println("Opcion no valida");
+      }
     }
+  }
 
+  private static double registrarTransaccion(Scanner scanner, double totalIngresos, double totalEgresos){
+    System.out.println("=================================================");
+    System.out.println("============== REGISTRAR TRANSACCION ============");
+    System.out.println("Por favor selecciona el tipo de transaccion: ");
+    System.out.println("1. Ingreso");
+    System.out.println("2. Egreso");
+    int tipoTransaccion = scanner.nextInt();
+    System.out.println("Por favor ingresa el monto de la transaccion: ");
+    double monto = scanner.nextDouble();
+    if(tipoTransaccion == 1){
+      totalIngresos += monto;
+      System.out.println("Ingreso registrado: " + monto);
+    }else if(tipoTransaccion == 2){
+      totalEgresos += monto;
+      System.out.println("Egreso registrado: " + monto);
+    }else{
+      System.out.println("Tipo de transaccion no valida");
+    }
+    return totalIngresos; // Devuelves los totales actualizados
+  }
 
+  private static void mostrarTransacciones(){
+    System.out.println("=================================================");
+    System.out.println("======= TRANSACCIONES REGISTRADAS ===============");
+    System.out.println("Ingreso: 1500");
+    System.out.println("Egreso: 300");
+    System.out.println("Ingreso: 2000");
+    System.out.println("Egreso: 500");
+  }
+
+  private static void calcularTotalOperaciones(double totalIngresos, double totalEgresos){
+    System.out.println("=================================================");
+    System.out.println("======= CALCULAR EL TOTAL DE OPERACIONES ========");
+    System.out.println("Total de Ingresos: " + totalIngresos);
+    System.out.println("Total de Egresos: " + totalEgresos);
+
+  }
+
+  private static void calcularBalance(double totalIngresos, double totalEgresos){
+    System.out.println("=================================================");
+    System.out.println("================= CALCULAR EL BALANCE ============");
+    System.out.println("Balance: " + (totalIngresos - totalEgresos));
   }
 }
